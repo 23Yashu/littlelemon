cbe688ac4ceb33ad98ba07a54ff1680707a46dce
Azkiel <azkiel007@gmail.com>
2025-07-10 21:18:44 +0530

    HTML Semantic structure

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+function Footer() {
+    return (
+        <footer>
+            <img src="./img/restaurant.jpg" alt="Little Lemon Restaurant" />
+            <h3>Little Lemon</h3>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <h3>Contact</h3>
+            <ul>
+                <li><p>You may also want to visit us:<br/>
+                </p>Little Lemon<br/>331 E Chicago<br/>
+                LaSalle Street Chicago,<br/>Illinois 60602
+                <br/>USA</li>
+                <li><p>jim@rock.com</p></li>
+                <li><p>(311) 555-2368</p></li>
+                <li><p>littlemon@bookings.com</p></li>
+            </ul>
+            <h3>Social Media</h3>
+            <ul>
+                <li><a href="https://www.facebook.com">
+                    <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 510.125"><path fill="#fff" fill-rule="nonzero" d="M512 256C512 114.615 397.385 0 256 0S0 114.615 0 256c0 120.059 82.652 220.797 194.157 248.461V334.229h-52.79V256h52.79v-33.709c0-87.134 39.432-127.521 124.977-127.521 16.218 0 44.202 3.18 55.651 6.36v70.916c-6.042-.635-16.537-.954-29.575-.954-41.977 0-58.196 15.901-58.196 57.241V256h83.619l-14.365 78.229h-69.254v175.896C413.771 494.815 512 386.885 512 256z"/></svg>
+                </a></li>
+                <li><a href="https://www.instagram.com">
+                    <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 510.125"><path fill="#fff" fill-rule="nonzero" d="M512 256C512 114.615 397.385 0 256 0S0 114.615 0 256c0 120.059 82.652 220.797 194.157 248.461V334.229h-52.79V256h52.79v-33.709c0-87.134 39.432-127.521 124.977-127.521 16.218 0 44.202 3.18 55.651 6.36v70.916c-6.042-.635-16.537-.954-29.575-.954-41.977 0-58.196 15.901-58.196 57.241V256h83.619l-14.365 78.229h-69.254v175.896C413.771 494.815 512 386.885 512 256z"/></svg>
+                </a></li>
+                <li><a href="https://www.twittercom">
+                    <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 510.125"><path fill="#fff" fill-rule="nonzero" d="M512 256C512 114.615 397.385 0 256 0S0 114.615 0 256c0 120.059 82.652 220.797 194.157 248.461V334.229h-52.79V256h52.79v-33.709c0-87.134 39.432-127.521 124.977-127.521 16.218 0 44.202 3.18 55.651 6.36v70.916c-6.042-.635-16.537-.954-29.575-.954-41.977 0-58.196 15.901-58.196 57.241V256h83.619l-14.365 78.229h-69.254v175.896C413.771 494.815 512 386.885 512 256z"/></svg>
+                </a></li>
+            </ul>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <img src="./img/Logo.svg" alt="Little Lemon"></img>
+            <meta name="description" content="We are a family owned
+Mediterranean restaurant, focused on traditional recipes served with a modern twist. "/>
+            <meta name="og:title" content="Little Lemon"/>
+            <meta name="og:description" content="We are a family owned
+Mediterranean restaurant, focused on traditional recipes served with a modern twist. "/>
+            <meta name="og:image" content="./img/restaurant.jpg"/>
+        </header>
+    )
+}
+export default Header;
