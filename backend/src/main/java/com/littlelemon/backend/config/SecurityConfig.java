65cabfeda417d7ab5ee0bf206c00e5f98ffd9678
yashasvi <vashisthayashasvi@gmail.com>
2026-03-30 12:52:57 +0530

    Fixing 403 forbidden error

--- backend/src/main/java/com/littlelemon/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/littlelemon/backend/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.littlelemon.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -16,7 +20,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register", "/api/bookings/**").permitAll()
                         .anyRequest().authenticated());
